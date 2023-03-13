715ea7ddae68fc47748561cc250d04a7f2b9f2a9
Dungwang Srisa-ard <dungwang.developer@protonmail.com>
2023-03-13 11:57:48 +0700

    update Joi schema

--- schemas.js
+++ schemas.js
@@ -1,13 +1,10 @@
 const Joi = require('joi')
 
-module.exports.ratingSchema = Joi.object({
-    rating: Joi.object({
-        teacher: Joi.string().required(),
-        school: Joi.string().required(),
-        department: Joi.string().required(),
-        comment: Joi.string(),
-        quality: Joi.number().integer().required().min(1).max(5),
-        difficulty: Joi.number().integer().required().min(1).max(5)
+module.exports.professorSchema = Joi.object({
+    professor: Joi.object({
+        first: Joi.string().required(),
+        last: Joi.string().required(),
+        department: Joi.string().required()
     }).required()
 })
 
