3c51cedc55ccdfea053f8799353aab20c93359f4
Dungwang Srisa-ard <dungwang.developer@protonmail.com>
2023-03-18 15:52:23 +0700

    add logout functionality

--- routes/users.js
+++ routes/users.js
@@ -30,4 +30,12 @@ router.post('/login', passport.authenticate('local', {failureFlash: true, failur
     res.redirect('/professors')
 })
 
+router.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      req.flash('success', "Goodbye!")
+      res.redirect('/professors')
+    })
+  })
+
 module.exports = router
